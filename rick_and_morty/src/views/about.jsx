7d96d70b3af2de9971bba92d40519999cd30e431
jgarcia19914 <63563869+jgarcia19914@users.noreply.github.com>
2023-08-08 10:56:56 -0500

    five

---
+++ rick_and_morty/src/views/about.jsx
@@ -0,0 +1,12 @@
+const about =()=> {
+    return(
+        <>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque alias enim consequuntur explicabo debitis soluta dolor sint sit, nulla laborum corporis iusto nobis recusandae ea error omnis. Optio, esse quisquam!</h1>
+        <p>prueba</p> 
+        <button>Ingresar</button>       
+        </>
+
+    )
+};
+
+export default about;
